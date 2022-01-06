6eb311a95d2a3552fedc7328d100bd8df5a9b2ff
OmShivamR24 <shivamrast06@gmail.com>
2022-01-07 03:42:48 +0530

    made changes to talker.cpp

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,4 +8,3 @@
 
 # modified environment variables
 export PWD='/home/shivamrastogi/catkin_workspace/build'
-export PYTHONPATH="/home/shivamrastogi/catkin_workspace/devel/lib/python3/dist-packages:$PYTHONPATH"
